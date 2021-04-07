cf17ab433a4997469639167edea065289330e2b2
Stephanie <stephanieschlaepfer@MacBook-Pro-3.local>
2021-04-07 13:18:06 +0200

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="Container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Open-source code, by Stephanie Schlaepfer
+      </footer>
+      </div>
     </div>
   );
 }
